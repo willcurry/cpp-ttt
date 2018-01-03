13c82cc9944632eced28c53eff815e2da604561c
willcurry <watercrafthd@gmail.com>
2018-01-03 14:08:20 +0000

    remove game tests since now untestable

--- ttt/gameTests.cpp
+++ ttt/gameTests.cpp
@@ -4,18 +4,3 @@
 #include "TestUtils.h"
 
 using namespace testing;
-
-TEST(Game, MakeMoveUpdatesBoard) {
-	Board board(TestUtils::CreateBoardState());
-	Game game(board);
-	game.MakeMove(0);
-	ASSERT_EQ(board.GetState()[0], 'x');
-}
-
-TEST(Game, SwitchesPlayersAfterMove) {
-	Board board(TestUtils::CreateBoardState());
-	Game game(board);
-	game.MakeMove(0);
-	game.MakeMove(1);
-	ASSERT_EQ(board.GetState()[1], 'o');
-}
